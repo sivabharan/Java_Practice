420126d38470d20228acff5c3c236bdac6679a52
vdurbhakula <vdurbhakula@homeaway.com>
2018-12-12 20:40:26 -0600

    palinrome updated with regex

--- src/main/java/JavaSamplePrograms/checkPalindrome.java
+++ src/main/java/JavaSamplePrograms/checkPalindrome.java
@@ -43,3 +43,25 @@ public class checkPalindrome {
 }
 
 
+/**
+ * public boolean isPalindromeRecursive(String text){
+ *     String clean = text.replaceAll("[\\W]|", "");
+ *     another way replaceAll("[^a-zA-Z]", "").toLowerCase();
+ *     return recursivePalindrome(clean,0,clean.length()-1);
+ * }
+ *
+ * private boolean recursivePalindrome(String text, int forward, int backward) {
+ *     if (forward == backward) {
+ *         return true;
+ *     }
+ *     if ((text.charAt(forward)) != (text.charAt(backward))) {
+ *         return false;
+ *     }
+ *     if (forward < backward + 1) {
+ *         return recursivePalindrome(text, forward + 1, backward - 1);
+ *     }
+ *
+ *     return true;
+ * }
+ */
+
