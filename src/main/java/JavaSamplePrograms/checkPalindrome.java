364903d5a29de517b48a880d7d8396fc2427ce65
vdurbhakula <vdurbhakula@homeaway.com>
2019-01-30 22:47:24 -0600

    Updated replace string logic

--- src/main/java/JavaSamplePrograms/checkPalindrome.java
+++ src/main/java/JavaSamplePrograms/checkPalindrome.java
@@ -51,7 +51,10 @@ public class checkPalindrome {
  *       String original, reverse = ""; // Objects of String class
  *       Scanner in = new Scanner(System.in);
  *       System.out.println("Enter a string/number to check if it is a palindrome");
+ *
  *       original = in.nextLine();
+ *       String original = text.replaceAll("[\\W]|", "");
+ *  *     another way replaceAll("[^a-zA-Z]", "").toLowerCase();
  *       int length = original.length();
  *       for ( int i = length - 1; i >= 0; i-- )
  *          reverse = reverse + original.charAt(i);
